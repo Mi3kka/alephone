b2ae90b0caebbf9ef15c4b6f560741510f2d0feb
Gregory Smith <wolfy@treellama.org>
2024-04-06 15:10:52 -0400

    refresh screen periodically while dialogs are visible; fixes Steam overlay

--- Source_Files/Misc/sdl_dialogs.cpp
+++ Source_Files/Misc/sdl_dialogs.cpp
@@ -2224,6 +2224,8 @@ int dialog::run(bool intro_exit_sounds)
 		if (machine_tick_count() > last_redraw + TICKS_PER_SECOND / 30)
 		{
 			draw_dirty_widgets();
+			SDL_Rect r{0.0, 0.0, rect.w, rect.h};
+			update(r);
 			last_redraw = machine_tick_count();
 		}
         
